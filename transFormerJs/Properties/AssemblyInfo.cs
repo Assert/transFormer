01af6b1fd00a113b88f1b59aee4f3ef537dfa296
Eystein Bye <eysteinbye@gmail.com>
2013-04-13 22:33:04 +0200

    Initial commit for transFormer.js

---
+++ transFormerJs/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("transFormerJs")]
+[assembly: AssemblyDescription("Easy to use Javascript two way binding framework.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Eystein Bye")]
+[assembly: AssemblyProduct("transFormerJs")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("734c4b48-2782-4d44-acff-287d39c0229b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
---
+++ transFormerJs/Scripts/transFormer.js
@@ -0,0 +1,141 @@
+﻿
+/*
+transFormer.js Framwork - Easy binding of form-data
+*/
+
+var transFormer = {
+
+    separator: " ",
+
+    // Gets value of attribute data-object, used for sub-objects like asset
+    getDataObject: function (elem) {
+        var dataObject = null;
+
+        for (var i = 0; i < elem.attributes.length; i++) {
+            if (elem.attributes[i].nodeName === "data-object") {
+                dataObject = elem.attributes[i].nodeValue;
+                break;
+            }
+        }
+        return dataObject;
+    },
+
+    // Gets value of attribute data-array, used for arrays of objects
+    getDataArrayDescriptor: function (elem) {
+        var dataArrayDescriptor = null;
+
+        for (var i = 0; i < elem.attributes.length; i++) {
+            if (elem.attributes[i].nodeName === "data-array") {
+                dataArrayDescriptor = elem.attributes[i].nodeValue;
+                break;
+            }
+        }
+        return dataArrayDescriptor;
+    },
+
+    populateModel: function (formData) {
+
+        var eventModel = function () {
+
+            this.makeObjectArrayFromString = function (str, descriptor) {
+                str = $.trim(str);
+                var arr = str.split(this.separator);
+                for (var x = 0; x < arr.length; x++) {
+                    var qq = $.trim(arr[x]);
+                    arr[x] = {};
+                    arr[x][descriptor] = qq;
+                }
+                return arr;
+            };
+        };
+
+        var event = new eventModel();
+
+        for (var i = 0; i < formData.length; i++) {
+            var elem = formData[i];
+
+            var value = elem.value;
+            if (value === "") value = null;
+
+
+            var arrDescriptor = this.getDataArrayDescriptor(elem);
+            if (arrDescriptor !== null) {
+                value = event.makeObjectArrayFromString(value, arrDescriptor);
+            }
+
+            var obj = this.getDataObject(elem);
+            if (obj !== null) {
+                if (typeof (event[obj]) === "undefined") {
+                    event[obj] = {};
+                }
+                event[obj][elem.id] = value;
+            } else {
+                event[elem.id] = value;
+            }
+        }
+        return event;
+    },
+
+    bind: function (data) {
+        /*
+        This binds value (to show)
+        */
+
+        var allTags = document.getElementsByTagName('*');
+        for (var i = 0; i < allTags.length; i++) {
+            var elem = allTags[i];
+
+            // for test
+            var vartype = typeof (elem);
+
+            var propName = elem.id;
+
+            if (propName === "id") propName = "Id";
+
+            var verdi = data[propName];
+
+            if (typeof (verdi) === "undefined") {
+                // Sub object
+                var objName = this.getDataObject(elem);
+
+                try {
+                    verdi = data[objName][propName];
+                } catch (e) {
+                    // html-element not found in data model
+                    continue;
+                }
+            }
+
+            if (typeof (verdi) === "object") {
+                // Array of objects
+                var str = "";
+                var descriptor = this.getDataArrayDescriptor(elem);
+                for (var j = 0; j < verdi.length; j++) {
+                    str += verdi[j][descriptor] + this.separator;
+                }
+                verdi = str;
+            }
+
+            if (elem.tagName === "DIV" || elem.tagName === "P") {
+                elem.innerHTML = verdi;
+            }
+            if (elem.tagName === "IMG") {
+                elem.src = verdi;
+            }
+            if (elem.tagName === "INPUT" || elem.tagName === "TEXTAREA") {
+                if (elem.type === 'checkbox') {
+                    elem.checked = verdi; // true false
+                } else if (elem.type === 'radio') {
+                    elem.checked = verdi; // true false
+                } else {
+                    elem.value = verdi;
+                }
+            }
+
+            if (elem.tagName === "SELECT") {
+                //var strUser = elem.options[elem.selectedIndex].value;
+                elem.value = verdi;
+            }
+        }
+    }
+};
